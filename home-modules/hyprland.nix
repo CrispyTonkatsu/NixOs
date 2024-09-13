773cba9ea563a7391b9a3209719457e43427d90b
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2024-09-13 00:13:07 -0700

    wallpaper

--- home-modules/hyprland.nix
+++ home-modules/hyprland.nix
@@ -28,20 +28,4 @@
 		};
 	};
 	
-	services.hyprpaper = {
-		enable = true;
-		settings = 
-			{
-				ipc = "on";
-				splash = false;
-				splash_offset = 2.0;
-
-				preload =
-					["/home/erina/Downloads/bocchipaper2.png" ];
-
-				wallpaper = [
-					"/home/erina/Downloads/bocchipaper2.png"
-				];
-			};
-	};
 }
--- home.nix
+++ home.nix
@@ -104,4 +104,21 @@
 	services = {
 		mako.enable = true;
 	};
+
+	services.hyprpaper = {
+		enable = true;
+		settings = 
+			{
+				ipc = "on";
+				splash = false;
+				splash_offset = 2.0;
+
+				preload =
+					["/home/erina/Downloads/bocchipaper2.png" ];
+
+				wallpaper = [
+					"/home/erina/Downloads/bocchipaper2.png"
+				];
+			};
+	};
 }
