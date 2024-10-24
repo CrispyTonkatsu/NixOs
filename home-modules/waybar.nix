3795cbde685efd76ffedad8f9c7fc99b796777fb
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2024-10-24 01:12:51 -0700

    Trying to get waybar to show the pretty

--- home-modules/waybar.nix
+++ home-modules/waybar.nix
@@ -1,6 +1,56 @@
-{lib, ...} : {
+{...} : {
 	programs.waybar = {
-		style = ./themes/waybar.css;
-		settings = lib.importJSON (./themes/waybar.json);
+		style = builtins.readFile ./themes/waybar.css;
+		settings = {
+			spacing = 10;
+			modules-left = [
+				"hyprland/window"
+			];
+			modules-right = [
+				"hyprland/language"
+				"network"
+				"battery"
+				"clock"
+				"tray"
+			];
+			"hyprland/window" = {
+				"max-length" = 50;
+			};
+			"hyprland/language" = {
+				format = "{}";
+				"format-en" = "en";
+				"format-es" = "sp";
+				"format-jp" = "jp";
+			};
+			battery = {
+				format = "{capacity}%";
+				"format-icons" = [
+					"\uf244"
+					"\uf243"
+					"\uf242"
+					"\uf241"
+					"\uf240"
+				];
+			};
+			clock = {
+				format = "{:%F %H:%M}";
+				"tooltip-format" = "{tz_list}";
+				timezones = [
+					"America/Vancouver"
+					"Asia/Manila"
+				];
+			};
+			network = {
+				format = "{ifname}";
+				"format-wifi" = "\uf1eb";
+				"format-ethernet" = "\udb80\ude97";
+				"format-disconnected" = "";
+				"max-length" = 50;
+			};
+			tray = {
+				"icon-size" = 21;
+				spacing = 10;
+			};
+		};
 	};
 }
