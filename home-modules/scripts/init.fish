f4d7475ffc3648ca5631ab8f80f78a03992f58fd
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2024-12-13 19:12:15 -0800

    Added a few shell commands

--- home-modules/scripts/init.fish
+++ home-modules/scripts/init.fish
@@ -1,9 +1,20 @@
 starship init fish | source
-set fish_greeting "We are so winning"
+set fish_greeting (random choice "We are so winning" "We are so losing")
+
 fish_default_key_bindings
 
-alias d="ninja -C ./build/debug/ -j(nproc)"
-alias dev="nix develop -c fish"
-alias nsp="nix-shell --run fish -p"
+abbr --add nsp nix-shell --run fish -p
+abbr --add dev nix develop -c fish
+abbr --add rebuild sudo nixos-rebuild switch --flake ~/.nixos/#default
 
-function nsp; nix-shell --run fish -p $argv; end
+function d -d "Build the ninja project in the current dir"
+	if test -d ./build/debug/
+		command ninja -C ./build/debug/ -j(nproc)
+	else
+		command ninja -C ./build/ -j(nproc)
+	end
+end
+
+function make-n -d "Call make up to the number provided"
+	command make (seq 0 $argv)
+end
