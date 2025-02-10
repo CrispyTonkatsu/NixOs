96b14cf6a27f17bbd2c2a478cbf19425c6c76152
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2025-02-10 12:13:03 -0800

    Created wallpaper folder

--- home-modules/hyprpaper.nix
+++ home-modules/hyprpaper.nix
@@ -6,10 +6,10 @@
     enable = true;
     settings = {
       preload =
-        [ "~/.nixos/home-modules/themes/tsubasa_cat.png" ];
+        [ "~/.nixos/home-modules/themes/wallpapers/tsubasa_cat.png" ];
 
       wallpaper =
-        [ ",~/.nixos/home-modules/themes/tsubasa_cat.png" ];
+        [ ",~/.nixos/home-modules/themes/wallpapers/tsubasa_cat.png" ];
     };
   };
 }
