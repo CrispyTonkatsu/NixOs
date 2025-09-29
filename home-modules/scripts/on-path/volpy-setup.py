69e0568855e3ee84b5fe2acf5dd2a7740af61fbf
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2025-09-28 18:47:54 -0700

    Almost done with the volpy script

--- home-modules/scripts/on-path/volpy-setup.py
+++ home-modules/scripts/on-path/volpy-setup.py
@@ -2,26 +2,35 @@
 
 import argparse, os
 from pathlib import Path
+from zipfile import ZipFile
 
 # Create the parser for an easier time dealing with the input arguments
 parser = argparse.ArgumentParser(
     description='automatically setup a volper project')
 
+default_path = str(Path.home()) + str('/digipen/CS330')
+
 # Adding the arguments the parser should look for
-_ = parser.add_argument('zip_path', type=Path, help='The location of the zip with the given files for the project')
-_ = parser.add_argument('-d','--destination', required=False, default='~/digipen/CS330', type=Path, help='The location to create the folder and setup the project')
+_ = parser.add_argument('zip_path', type=str, help='The location of the zip with the given files for the project')
+_ = parser.add_argument('-d','--destination', required=False, default=default_path, type=str, help='The location to create the folder and setup the project')
 
 # Collecting the arguments
 args = parser.parse_args()
+zip_path = str(args.zip_path)
+destination = str(args.destination)
 
 # Processing the strings to create the folder
-folder_name = str(os.path.basename(args.zip_path))
+file_name = str(os.path.basename(zip_path))
 
-# Checking that we are getting a zip
-if(not folder_name.endswith(".zip")):
-    print("zip path does not evaluate to zip file")
+# Checking that we are getting a zip file that exists
+if(not file_name.endswith(".zip") or not os.path.isfile(zip_path)):
+    print("zip path does not evaluate to existing zip file")
     exit()
 
-folder_path = str(args.destination) + "/" + folder_name
+folder_path = str(destination) + "/" + file_name.removesuffix(".zip")
 
-print(folder_path)
+# Actually setting up the environment
+os.mkdir(folder_path)
+ZipFile(zip_path).extractall(folder_path)
+
+# Cloning the github template
