a5ea737245ad5284580ab5a742bc6e03f1c0bc2e
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-06-17 17:22:21 -0700

    Fixed greeter

--- core-modules/greetd.nix
+++ core-modules/greetd.nix
@@ -4,8 +4,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --greet 'fleeting' --asterisks --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' --cmd start-hyprland";
-        user = "erina";
+        command = "${pkgs.tuigreet}/bin/tuigreet -t -g 'fleeting' --asterisks --cmd start-hyprland";
       };
     };
   };
