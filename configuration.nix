124d726b21e3b16ac4f86f5b53bbd79f6c4a7d49
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-07-12 14:34:46 -0700

    Removed upgrade service

--- configuration.nix
+++ configuration.nix
@@ -25,8 +25,6 @@
   # nixos store optimization
   nix.optimise.automatic = true;
 
-  system.autoUpgrade.enable = true;
-
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
