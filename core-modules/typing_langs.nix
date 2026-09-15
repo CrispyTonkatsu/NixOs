a295971c258eea131e92e6fed71834fbb77f1660
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-09-14 21:32:05 -0700

    Adjusting keyboards

--- core-modules/typing_langs.nix
+++ core-modules/typing_langs.nix
@@ -7,10 +7,27 @@
     type = "fcitx5";
     enable = true;
     fcitx5.waylandFrontend = true;
+
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
       fcitx5-gtk
       fcitx5-rose-pine
     ];
+
+    fcitx5.settings = {
+      inputMethod = {
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "keyboard-us-intl";
+        };
+        "Groups/0/Items/0" = {
+          Name = "keyboard-us-intl";
+        };
+        "Groups/0/Items/1" = {
+          Name = "mozc";
+        };
+      };
+    };
   };
 }
